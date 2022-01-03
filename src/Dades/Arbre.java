81044b6294d3297dab22f531392ff9a043c96e73
Adrian Garcia <56340820+Agvi32@users.noreply.github.com>
2022-01-03 16:26:49 +0100

    Añado todos los archivos

---
+++ src/Dades/Arbre.java
@@ -0,0 +1,8 @@
+package Dades;
+
+public class Arbre extends Planta  {
+	private int[] rangs;
+	private int edatactual;
+	
+	// se har� el c�lculo dependiendo del rango de edades de cada arbol y en que rango se esta actualmente
+}
