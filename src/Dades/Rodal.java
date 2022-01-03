45905b13668172d99d717f8a23cf0635145506fa
Cristhian.cfg <cristhianfg.cfg@gmail.com>
2022-01-03 15:34:49 +0100

    Rama Main BASE CON TODAS LAS CLASES

---
+++ src/Dades/Rodal.java
@@ -0,0 +1,26 @@
+package Dades;
+
+public class Rodal {
+	private TipusTerreny tipus;
+	private double superficie;
+	
+	public Rodal (TipusTerreny tipus, double superficie) {
+		this.tipus = tipus;
+		this.superficie = superficie;
+	}
+	
+	public TipusTerreny getTipus() {
+		return tipus;
+	}
+	public void setTipus(TipusTerreny tipus) {
+		this.tipus = tipus;
+	}
+	public double getSuperficie() {
+		return superficie;
+	}
+	public void setSuperficie(double superficie) {
+		this.superficie = superficie;
+	}
+	
+	
+}
